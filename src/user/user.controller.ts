92ab062d9f5ef3a609f1a7846f3e6a001e3914b1
devga <biennguyen131311@gmail.com>
2023-03-30 15:44:19 +0700

    user decorator

---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, UseGuards, Patch, Body, Req } from '@nestjs/common';
+import { Strategy } from 'passport-jwt';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
+import { UserService } from './user.service';
+import { EditUserDto } from './dto';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
+}
